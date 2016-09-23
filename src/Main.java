0ee326debe7dca567019879ab4704f9f0ab93d19
Luccas Silveira <luccas.carsil@gmail.com>
2016-09-23 12:06:17 -0300

    Implementação do parse dos comandos enviados para as rovers.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		Control control = new Control();
 		control.readCommands();
+		if (control.parseCommands()) {
+			System.out.println("HELL YEAH!");
+		} else {
+			System.out.println("ERROR!");
+		}
 	}
 
 }
--- src/nasa/Control.java
+++ src/nasa/Control.java
@@ -1,6 +1,8 @@
 package nasa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import mars.*;
@@ -11,41 +13,95 @@ public class Control {
 
 	private Plateau plateau;
 	private ArrayList<Rover> rovers;
+	private ArrayList<String> movements;
 
-	private String plateauDimensions;
-	private ArrayList<String> roversCommands;
+	private ArrayList<String> commands;
+
+	private List<String> validOrientations;
+	private List<String> validMovements;
 
 	public Control() {
 		this.scanner = new Scanner(System.in);
-		this.roversCommands = new ArrayList<String>();
+
+		this.validOrientations = Arrays.asList("N", "W", "S", "E");
+		this.validMovements = Arrays.asList("L", "R", "M");
+
+		this.movements = new ArrayList<String>();
 		this.rovers = new ArrayList<Rover>();
 
+		this.commands = new ArrayList<String>();
 	}
 
 	public void readCommands() {
-		this.roversCommands.clear();
+		this.commands.clear();
 		String command = "";
 
+		System.out.println("*** MARS ROVERS ***");
 		System.out.println("Insira os comandos:");
-		this.plateauDimensions = scanner.next();
 
-		while(!command.equals("end")) {
-			command = scanner.next();
-			if (!command.equals("end")) {
-				this.roversCommands.add(command);
+		while(!command.equals("END")) {
+			command = scanner.nextLine();
+			if (!command.equals("END")) {
+				this.commands.add(command);
 			}
 		}
-
-		this._parseCommands();
 	}
 
-	private void _parseCommands() {
-		String[] plateau_dimensions = new String[2];
-		plateau_dimensions = this.plateauDimensions.split(" ");
-		this.plateau = new Plateau(Integer.parseInt(plateau_dimensions[0]), Integer.parseInt(plateau_dimensions[1]));
+	public boolean parseCommands() {
+		if (this.commands.size() > 1 && this.commands.size() % 2 == 1) {
+			try {
+				// Plateau
+				String[] plateau_dimensions = new String[2];
+				plateau_dimensions = this.commands.remove(0).split(" ");
+				int plateau_max_x = Integer.parseInt(plateau_dimensions[0]);
+				int plateau_max_y = Integer.parseInt(plateau_dimensions[1]);
+	
+				if (plateau_max_x > 0 && plateau_max_y > 0) {
+					this.plateau = new Plateau(plateau_max_x, plateau_max_y);
+				} else {
+					return false;
+				}
+	
+				// Rovers
+				String[] rover_deployment = new String[3];
+				String command;
+				int rover_deployment_x;
+				int rover_deployment_y;
+				String rover_deployment_orientation;
+	
+				for (int n = 0; n < this.commands.size(); n += 2) {
+					command = this.commands.get(n);
+	
+					rover_deployment = command.split(" ");
+					rover_deployment_x = Integer.parseInt(rover_deployment[0]);
+					rover_deployment_y = Integer.parseInt(rover_deployment[1]);
+					rover_deployment_orientation = rover_deployment[2];
 
-		for (String rover_command : this.roversCommands) {
-			
+					if (rover_deployment_x >= 0 && rover_deployment_x < plateau_max_x && rover_deployment_y >= 0 && rover_deployment_y < plateau_max_y && this.validOrientations.contains(rover_deployment_orientation)) {
+						this.rovers.add(new Rover(rover_deployment_x, rover_deployment_y, rover_deployment_orientation, this.plateau));
+					} else {
+						return false;
+					}
+				}
+
+				// Movements
+				String movement;
+				for (int n = 1; n < this.commands.size(); n += 2) {
+					movement = this.commands.get(n);
+					for (char c : movement.toCharArray()) {
+						if (!this.validMovements.contains(String.valueOf(c))) {
+							return false;
+						}
+					}
+					this.movements.add(movement);
+				}
+
+				return true;
+			} catch (Exception e) {
+				return false;
+			}
+		} else {
+			return false;
 		}
 	}
 }
